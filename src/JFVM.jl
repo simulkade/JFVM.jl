234dcb77a76c3bc99fbca54e1c007d2a9cd7a257
AAE <e.eftekhari@gmail.com>
2015-01-11 17:38:36 +0100

    MUMPS solver, needs MUMPS.jl to be installed

--- src/JFVM.jl
+++ src/JFVM.jl
@@ -11,7 +11,7 @@ export MeshStructure, BoundaryCondition, CellValue, FaceValue,
        convectionTerm, convectionTvdTerm, diffusionTerm, createCellVariable,
        createFaceVariable, copyCell, fluxLimiter, createMesh1D,
        createMesh2D, createMesh3D, createMeshRadial2D, createMeshCylindrical2D,
-       createMeshCylindrical3D, solveLinearPDE, visualizeCells, 
+       createMeshCylindrical3D, solveLinearPDE, visualizeCells, solveMUMPSLinearPDE,
        linearSourceTerm, constantSourceTerm, transientTerm
 
 include("fvmToolTypes.jl")
