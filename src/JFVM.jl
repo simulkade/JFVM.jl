1911db0f0f2f49f3f0ce8fa52be9e0858c4de10f
AAE <e.eftekhari@gmail.com>
2015-01-12 16:46:25 +0100

    exported cylindrical mesh 1D

--- src/JFVM.jl
+++ src/JFVM.jl
@@ -11,8 +11,8 @@ export MeshStructure, BoundaryCondition, CellValue, FaceValue,
        convectionTerm, convectionTvdTerm, diffusionTerm, createCellVariable,
        createFaceVariable, copyCell, fluxLimiter, createMesh1D,
        createMesh2D, createMesh3D, createMeshRadial2D, createMeshCylindrical2D,
-       createMeshCylindrical3D, solveLinearPDE, visualizeCells, solveMUMPSLinearPDE,
-       linearSourceTerm, constantSourceTerm, transientTerm
+       createMeshCylindrical3D, createMeshCylindrical1D, solveLinearPDE, visualizeCells,
+       linearSourceTerm, constantSourceTerm, transientTerm, solveMUMPSLinearPDE
 
 include("fvmToolTypes.jl")
 include("meshstructure.jl")
